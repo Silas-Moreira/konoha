9fdaf4642182876ff2539db33689014d10ce0cf9
Silas <silasmoreira1@gmail.com>
2022-09-05 13:57:27 -0300

    criei menu hamburguer

---
+++ script.js
@@ -0,0 +1,17 @@
+const btMobile = document.getElementById("bt-mobile");
+
+function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefault()
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+    const active = nav.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", active);
+    if (active) { 
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");   
+    }
+}
+
+btMobile.addEventListener("click", toggleMenu);
+btMobile.addEventListener("touchstart", toggleMenu);
